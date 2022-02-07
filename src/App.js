a6245dd5c795bbc2a09c1f08ce6aeacf5831bf3e
Dario Prazeres <darioedgarprazeres@gmail.com>
2022-02-08 00:25:38 +0100

    footer create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import RouteSwicth from "../src/pages/RouterSwicth";
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
         <React.StrictMode>
           <RouteSwicth/>
         </React.StrictMode>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Footer = ()=>{
+    return(
+        <footer className="bg-light">
+            <p>&copy;Created by DPpoject</p> 
+        </footer>
+    );
+}
+export default Footer;
